92fe107038ebff51cd941ad4e0c2b7117852f2bd
achilles schmelzer <a@achilles.engineering>
2024-04-06 18:14:54 -0700

    Add next project for site

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-neutral-950 text-9xl font-bold">GADFLY</h1>
+    </main>
+  );
+}
