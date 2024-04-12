4fc80356b467e0d2517a653400761d3ca92682d6
achilles schmelzer <a@achilles.engineering>
2024-04-12 11:13:00 -0700

    Add Prompt to Stream

---
+++ apps/engine/src/network/stream/Prompt.ts
@@ -0,0 +1,3 @@
+export type Prompt = {
+  text: string;
+};
--- apps/engine/src/network/stream/Stream.ts
+++ apps/engine/src/network/stream/Stream.ts
@@ -1,7 +1,9 @@
 import { Address } from "../address/Address.js";
 import { Message } from "../message/Message.js";
+import { Prompt } from "./Prompt.js";
 
 export type Stream = {
   address: Address;
+  prompt: Prompt;
   history: Message[];
 };
