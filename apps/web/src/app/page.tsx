1298d3531d9171818f219f64e9962bea89fc352f
achilles schmelzer <a@achilles.engineering>
2024-04-16 15:56:34 -0700

    Update site

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-neutral-950 text-9xl font-bold">GADFLY</h1>
+    <main className="main">
+      <Image
+        className="logo"
+        src="/gadfly.png"
+        alt="Gadfly"
+        width={109.2}
+        height={128.8}
+      />
+      <h1>gadfly</h1>
+      <h2>a system 2 compiler for language models</h2>
+      <nav>
+        <a href="https://github.com/killthebuddh4">github</a>
+        <a href="https://twitter.com/killthebuddha_">twitter</a>
+        <a href="https://discord.gg/Ad5szmQWhS">discord</a>
+      </nav>
     </main>
   );
 }
