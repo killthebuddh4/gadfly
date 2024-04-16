4336d298521669e3119742bf889effe653ec7ed1
achilles schmelzer <a@achilles.engineering>
2024-04-15 23:40:16 -0700

    Add a couple sestina parser test cases

--- apps/engine/src/networks/sestina/parser/parseSestina.test.js
+++ apps/engine/src/networks/sestina/parser/parseSestina.test.js
@@ -2,14 +2,22 @@ import { parseSestina } from "./parseSestina.js";
 import { readFile } from "fs/promises";
 
 describe("parseSestina", () => {
-  it.only("should parse a sestina", async () => {
-    const text = await readFile("./.data/sestina.txt", "utf-8");
+  it("parses bishop-sestina.txt", async () => {
+    const text = await readFile("./data/bishop-sestina.txt", "utf-8");
 
     const result = parseSestina({ text });
 
-    console.log(result);
-
     if (!result.ok) {
+      throw new Error(result.reason);
+    }
+  });
+
+  it("fails to parse pound-sestina-altaforte.txt", async () => {
+    const text = await readFile("./data/pound-sestina-altaforte.txt", "utf-8");
+
+    const result = parseSestina({ text });
+
+    if (result.ok) {
       throw new Error(result.reason);
     }
   });
