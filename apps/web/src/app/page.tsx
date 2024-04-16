fdc06a7345f384aae347b50b6ade71b092790b0c
achilles schmelzer <a@achilles.engineering>
2024-04-16 16:48:19 -0700

    Add link to deck

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+const SEED_DECK_URL =
+  "https://www.canva.com/design/DAGCbH7Q9nw/cWEMoBESe1JUii0SxWhIRA/view?utm_content=DAGCbH7Q9nw&utm_campaign=designshare&utm_medium=link&utm_source=editor";
+
 export default function Home() {
   return (
     <main className="main">
@@ -17,6 +20,9 @@ export default function Home() {
         <a href="https://twitter.com/killthebuddha_">twitter</a>
         <a href="https://discord.gg/Ad5szmQWhS">discord</a>
       </nav>
+      <a href={SEED_DECK_URL} className="deck">
+        seed deck
+      </a>
     </main>
   );
 }
