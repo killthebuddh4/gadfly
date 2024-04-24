553a924b95a5005d94b0d907f226379ecdbbb421
achilles schmelzer <a@achilles.engineering>
2024-04-24 11:41:35 -0700

    Remove some superfluous code

--- apps/engine/src/primitives/buildNetwork.test.js
+++
@@ -1,9 +0,0 @@
-import { buildNetwork } from "./buildNetwork.js";
-
-describe("buildNetwork", () => {
-  it.only("should build a network from a list of primitives", async function () {
-    this.timeout(60000);
-
-    await buildNetwork();
-  });
-});
--- apps/engine/src/primitives/createOnParentChange.ts
+++
@@ -1,3 +0,0 @@
-export const createOnParentChange = () => {
-  return null;
-};
