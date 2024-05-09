523dafb9fff73c067eab2bcba8c017bc9aa5a26d
achilles schmelzer <a@achilles.engineering>
2024-05-09 12:47:48 -0700

    Dialogue, Actor, Message

--- apps/engine/src/protocol/engine/Dialectic.ts
+++
@@ -1,9 +0,0 @@
-import { Flow } from "../structures/Flow.js";
-import { Machine } from "../structures/Machine.js";
-import { Actor } from "./Actor.js";
-
-export type Dialectic = {
-  flow: () => Promise<Flow>;
-  machine: () => Promise<Machine>;
-  actors: () => Promise<Actor[]>;
-};
