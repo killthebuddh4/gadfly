6bf3ff074df420068ff549e6f7905b70549fc9f9
achilles schmelzer <a@achilles.engineering>
2024-05-15 13:20:16 -0700

    Add app package

---
+++ apps/app/README.md
@@ -0,0 +1,1 @@
+# https://gadfly.run
---
+++ apps/app/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+
+export const metadata: Metadata = {
+  title: "Gadfly UI",
+  description: "A system 2 for language models.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
