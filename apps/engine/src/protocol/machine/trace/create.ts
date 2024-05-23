3d054a2f28ecfb958e0930cffce4be08ba5cf992
achilles schmelzer <a@achilles.engineering>
2024-05-23 16:20:41 -0700

    Next up Actor and Log

--- apps/engine/src/protocol/machine/trace/create.ts
+++ apps/engine/src/protocol/machine/trace/create.ts
@@ -1,9 +1,16 @@
 import { z } from "zod";
 import { create as sequenceCreate } from "../../structures/sequence/create.js";
-import { zCreateRootBody } from "../../structures/sequence/api/schemas.js";
+import { create as pointerCreate } from "../../primitives/pointer/create.js";
+import { zCreateRootBody as zCreatePointerBody } from "../../primitives/pointer/api/schemas.js";
+import { zCreateRootBody as zCreateSequenceBody } from "../../structures/sequence/api/schemas.js";
 
-export const create = async (args: z.infer<typeof zCreateRootBody>) => {
-  const sequence = await sequenceCreate(args);
+export const create = async (args: {
+  machineId: string;
+  pointer: z.infer<typeof zCreatePointerBody>;
+  sequence: z.infer<typeof zCreateSequenceBody>;
+}) => {
+  const sequence = await sequenceCreate(args.sequence);
+  const pointer = await pointerCreate(args.pointer);
 
-  return { trace: sequence };
+  return { trace: sequence, pointer };
 };
